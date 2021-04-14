386305bf03daf429491d75686c169f24cbdc8b4a
Danilo Ribeiro <danriba@gmail.com>
2021-04-14 16:18:52 -0300

    First Commit

--- src/test_notas.py
+++ src/test_notas.py
@@ -3,19 +3,20 @@ import pytest
 from notas import Notas
 
 def test_notas_maior_que_zero_np1enp2():
-    notas = Notas(2,50)
-    assert (notas.p1 >= 0, "values has to be positive")
+    notas = Notas(-2,50)
+    print(notas.p1)
+    assert ((notas.p1 < 0) or (notas.p2 < 0)), "values has to be positive"
 
 def test_notas_maior_que_zero_np1enp2enp3():
-    notas = Notas(30,50, 2)
-    assert ((notas.p1 >= 0) and (notas.p2 >= 0) and (notas.p3 >= 0)), "values has to be positive"
+    notas = Notas(30,50,-2)
+    assert ((notas.p1 < 0) or (notas.p2 < 0) or (notas.p3 < 0)), "values has to be positive"
 
 def test_np1enp3_diferente_none():
     notas = Notas(1,1)
     assert not ((notas.p1 == None) or (notas.p2 == None)), "p1 and p2 shouldn't be None"
 
 def test_aprovado_sem_np3():
-    notas = Notas(59,60)
+    notas = Notas(60,59)
     assert notas.is_aprovado(), "average should be >= 60"
 
 def test_aprovado_com_np3():
@@ -30,7 +31,6 @@ def test_reprovado_sem_np3():
 def test_reprovado_com_np3():
     notas = Notas(49,49,49)
     assert not notas.is_aprovado(), "average should be < 50"
-
 
 #pytest.mark.parametrize("inicial,final", [(0,0),(1,0),(2,1)])
 
